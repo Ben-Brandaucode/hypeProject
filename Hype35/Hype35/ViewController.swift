966a8085f44abacf07e4e03817a5e6c96706a82e
Benjamin Brandau <codingbrandau@gmail.com>
2020-08-20 16:11:39 -0600

    folders set up

---
+++ Hype35/Hype35/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hype35
+//
+//  Created by Ben Brandau Brandau on 8/20/20.
+//  Copyright © 2020 Ben Brandau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
